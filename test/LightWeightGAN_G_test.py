c9e48b234067e14d94b7dd88b5277a6af28d390c
Ren634 <ren.putiputi.634@gmail.com>
2021-09-06 01:13:48 +0900

    add image files to test

--- test/LightWeightGAN_G_test.py
+++ test/LightWeightGAN_G_test.py
@@ -3,10 +3,13 @@ import numpy as np
 import torch
 from LightWeightGAN import Generator
 #%%
+resolution = 128
 device = "cuda" if torch.cuda.is_available else "cpu"
-x = torch.randn((1,256,1,1),device=device)
-netG = Generator(256,128).to(device)
-y = netG(x)
+netG = Generator(100,resolution).to(device)
+#%%
+x = torch.randn((1,100,1,1),device=device)
+for _ in range(5):
+    y = netG(x)
 
 # %%
 

d5fb8ec3152d9588ee8fda2225f35e6d9b28ed27
Ren634 <ren.putiputi.634@gmail.com>
2021-09-10 17:15:26 +0900

    add train functions and support for GPU

--- PGGAN.py
+++ PGGAN.py
@@ -1,8 +1,13 @@
 import torch
 from torch import nn
 from gan_modules import *
+from tqdm.notebook import tqdm
+from torchvision.transforms import functional as TF
+from torch.utils import tensorboard
+from math import log2
 from collections import deque
 
+device = "cuda" if torch.cuda.is_available else "cpu"
 
 class InputG(nn.Module):
     def __init__(self,in_channels,out_channels,negative_slope=0.1,is_spectral_norm=True):
@@ -17,10 +22,10 @@ class InputG(nn.Module):
         self.main = nn.Sequential(
             ConvT2d(in_channels=in_channels,out_channels=out_channels,kernel_size=(4,4)),
             PixelNorm2d(),
-            nn.LeakyReLU(negative_slope=negative_slope,inplace=True),
+            nn.LeakyReLU(negative_slope=negative_slope),
             Conv2d(in_channels=in_channels,out_channels=out_channels,kernel_size=(3,3),padding=1),
             PixelNorm2d(),
-            nn.LeakyReLU(negative_slope=negative_slope,inplace=True)
+            nn.LeakyReLU(negative_slope=negative_slope)
         )
     def forward(self,inputs):
         output = self.main(inputs)
@@ -37,10 +42,10 @@ class BlockG(nn.Module):
             nn.Upsample(scale_factor=2,mode="nearest"),
             Conv2d(in_channels=in_channels,out_channels=out_channels,kernel_size=(3,3),padding=1),
             PixelNorm2d(),
-            nn.LeakyReLU(negative_slope=negative_slope,inplace=True),
+            nn.LeakyReLU(negative_slope=negative_slope),
             Conv2d(in_channels=out_channels,out_channels=out_channels,kernel_size=(3,3),padding=1),
             PixelNorm2d(),
-            nn.LeakyReLU(negative_slope=negative_slope,inplace=True),
+            nn.LeakyReLU(negative_slope=negative_slope),
         )
         
     def forward(self,x):
@@ -67,7 +72,7 @@ class RGBAdd(nn.Module):
 class Generator(nn.Module):
     def __init__(self,n_dims,max_resolution,negative_slope=0.1,is_spectral_norm=True):
         super().__init__()
-        self.img_size = torch.tensor(4)
+        self.img_size = torch.tensor(4.0)
         self.register_buffer("image_size",self.img_size)
         self.max_resolution = max_resolution
         self.is_spectral_norm = is_spectral_norm
@@ -113,32 +118,30 @@ class Generator(nn.Module):
         
     def update(self):
         img_size = self.img_size.item()
-        self.main.append(
+        self.main.append(    
             BlockG(
                 in_channels=self.out_channels[img_size],
                 out_channels=self.out_channels[img_size*2],
                 negative_slope=self.negative_slope,
                 is_spectral_norm=self.is_spectral_norm
-                )
+                ).to(device)
             )
         if(self.is_spectral_norm):
             Conv2d = sn_conv2d
-            ConvT2d = sn_tconv2d
         else:
             Conv2d = EqualizedLRConv2d
-            ConvT2d = EqualizedLRTConv2d
              
         self.to_RGB["old"] = nn.Sequential(
             nn.Upsample(scale_factor=2,mode="nearest"),
-            self.to_RGB["up_to_date"])
-        self.to_RGB["up_to_date"] = Conv2d(in_channels=self.out_channels[img_size*2],out_channels=3,kernel_size=(1,1))
+            self.to_RGB["up_to_date"]).to(device)
+        self.to_RGB["up_to_date"] = Conv2d(in_channels=self.out_channels[img_size*2],out_channels=3,kernel_size=(1,1)).to(device)
         self.img_size *= 2
         
     def forward(self,x):
         RGBs= []
         for layer in self.main: 
             x = layer(x)
-            if(self.img_size//2 == x.shape[-1]):
+            if(self.img_size.item()//2 == x.shape[-1]):
                 RGBs.append(self.to_RGB["old"](x))
         RGBs.append(self.to_RGB["up_to_date"](x))
         output = self.output_layer(RGBs)
@@ -153,9 +156,9 @@ class BlockD(nn.Module):
             Conv2d = EqualizedLRConv2d
         self.main = nn.Sequential(
             Conv2d(in_channels=in_channels,out_channels=out_channels,kernel_size=(3,3),padding=1),
-            nn.LeakyReLU(negative_slope=negative_slope,inplace=True),
-            Conv2d(in_channels=in_channels,out_channels=out_channels,kernel_size=(3,3),padding=1),
-            nn.LeakyReLU(negative_slope=negative_slope,inplace=True),
+            nn.LeakyReLU(negative_slope=negative_slope),
+            Conv2d(in_channels=out_channels,out_channels=out_channels,kernel_size=(3,3),padding=1),
+            nn.LeakyReLU(negative_slope=negative_slope),
             nn.AvgPool2d(kernel_size=(2,2))
         )
     def forward(self,inputs):
@@ -190,16 +193,16 @@ class Discriminator(nn.Module):
         self.fromRGB = nn.ModuleDict({
             "up_to_date":nn.Sequential(
             Conv2d(in_channels=3,out_channels=self.out_channels[4],kernel_size=(1,1)),
-            nn.LeakyReLU(negative_slope=negative_slope,inplace=True)
+            nn.LeakyReLU(negative_slope=negative_slope)
             )
         })
-        self.main = nn.ModuleList([])
+        self.main = nn.ModuleList()
         self.output_layer = nn.Sequential(
             MiniBatchStddev(),
             Conv2d(in_channels=self.out_channels[4]+1,out_channels=self.out_channels[4],kernel_size=(3,3),padding=1),
-            nn.LeakyReLU(negative_slope=negative_slope,inplace=True),
+            nn.LeakyReLU(negative_slope=negative_slope),
             Conv2d(in_channels=self.out_channels[4],out_channels=self.out_channels[4],kernel_size=(4,4)),
-            nn.LeakyReLU(negative_slope=negative_slope,inplace=True),
+            nn.LeakyReLU(negative_slope=negative_slope),
             nn.Flatten(),
             Linear(in_features=self.out_channels[4],out_features=1)
             )
@@ -224,7 +227,7 @@ class Discriminator(nn.Module):
         self.fromRGB["old"] = nn.Sequential(
             nn.AvgPool2d(2,2),
             self.fromRGB["up_to_date"]
-        )
+        ).to(device)
         
         self.fromRGB["up_to_date"] = nn.Sequential(
             Conv2d(
@@ -232,9 +235,9 @@ class Discriminator(nn.Module):
                 out_channels=self.out_channels[img_size*2],
                 kernel_size=(1,1)
                 ),
-            nn.LeakyReLU(negative_slope=self.negative_slope,inplace=True)
-        ) 
-        self.main.insert(0,BlockD(in_channels=self.out_channels[img_size*2],out_channels=self.out_channels[img_size]))
+            nn.LeakyReLU(negative_slope=self.negative_slope)
+        ).to(device)
+        self.main.insert(0,BlockD(in_channels=self.out_channels[img_size*2],out_channels=self.out_channels[img_size]).to(device))
         self.img_size *= 2
          
     def forward(self,x):
@@ -260,12 +263,92 @@ class PGGAN(GAN):
         d_betas=(0,0.999),
         g_betas=(0,0.999),
         negative_slope=0.1,
-        is_spectral_norm=True
+        is_spectral_norm=True,
+        loss ="wasserstein",
         ):
         super().__init__()            
-        self.netD = Discriminator(negative_slope=negative_slope,is_spectral_norm=is_spectral_norm)
-        self.netG = Generator(n_dims=n_dims,max_resolution=max_resolution,negative_slope=negative_slope,is_spectral_norm=is_spectral_norm)
+        self.max_resolution = max_resolution
+        self.netD = Discriminator(negative_slope=negative_slope,is_spectral_norm=is_spectral_norm).to(device)
+        self.netG = Generator(n_dims=n_dims,max_resolution=max_resolution,negative_slope=negative_slope,is_spectral_norm=is_spectral_norm).to(device)
         self.optimizer_d = torch.optim.Adam(self.netD.parameters(),lr=d_lr,betas=d_betas)
         self.optimizer_g = torch.optim.Adam(self.netG.parameters(),lr=g_lr,betas=g_betas)
-
+        self.loss = WassersteinGP(self.netD) if loss =="wasserstein" else Hinge() 
+        
+    def train_d(self,real_img,fake_img):
+        self.optimizer_d.zero_grad()
+        real = self.netD(real_img)
+        fake = self.netD(fake_img)
+        loss = self.loss(real,fake,real_img,fake_img)
+        loss.backward()
+        self.optimizer_d.step()
+        return loss 
     
+    def train_g(self,fake_img):
+        self.optimizer_g.zero_grad()
+        fake = self.netD(fake_img)
+        loss = -torch.mean(fake)
+        loss.backward()
+        self.optimizer_g.step()
+        return loss
+    
+    def train(self,loader,epochs,img_size,save_num,is_tensorboard): 
+        for epoch in tqdm(range(epochs),desc="Epochs",total=epochs+self.total_epochs,initial=self.total_epochs,leave=False):
+            for step,data in enumerate(tqdm(loader,desc="Steps",leave=False),start=1):
+                real_imgs,_ = data
+                real_imgs = TF.resize(real_imgs,size=img_size).to(device)
+                noise = torch.randn(size=(real_imgs.shape[0],self.n_dims,1,1),device=device)
+                fake_imgs = self.netG(noise,True)
+                loss_d = self.train_d(real_imgs,fake_imgs).to("cpu")
+                self.total_steps += 1
+                if(self.total_steps % self.n_dis ==0):
+                    loss_g = self.train_g(fake_imgs).to("cpu")
+                else:
+                    with torch.no_grad():
+                        output = self.netD(fake_imgs)
+                    loss_g = -torch.mean(output).to("cpu")
+                    del output
+                if(is_tensorboard):
+                    self.log_writer.add_scalars(
+                        "Loss",
+                        {"Generator":loss_g.item(),"Discriminator":loss_d.item()},
+                        global_step=self.total_steps
+                        )                    
+                if(step % save_num == 0):
+                    with torch.no_grad():
+                        sample_img = (self.netG(self.fixed_noise).detach() + 1) / 2
+                        sample_img = sample_img.to("cpu")
+                    save_img(sample_img, file_name=f"size_{img_size}epoch{self.total_epochs}_step{step}")
+                    for i in range(5):
+                        check_vec = torch.randn(size=(1,self.n_dims,1,1),device=device)
+                        with torch.no_grad():
+                            check_img = (torch.squeeze(self.netG(check_vec)).detach() + 1) / 2
+                            check_img = check_img.to("cpu")
+                        save_img(check_img, file_name=f"size_{img_size}epoch{self.total_epochs}_step{step}_check{i}",is_grid=False)
+                    else:
+                        del check_vec 
+            self.total_epochs += 1
+            if((epoch+1) % 10 == 0):
+                self.save_model(f"params\size{img_size}_epoch_{self.total_epochs-1}")
+            self.netD.update()
+            self.netG.update()
+
+    def fit(self,dataset,epochs,batch_size=10,shuffle=True,num_workers=0,is_tensorboard=True,image_num=10):
+        if(is_tensorboard):
+            self.log_writer = tensorboard.SummaryWriter(log_dir="./logs")
+        self.fixed_noise = torch.randn(size=(16,self.n_dims,1,1),device=device)
+        current_size = self.netG.img_size.item()
+        begin = int(log2(current_size))
+        end = int(log2(self.max_resolution))
+        if(not isinstance(epochs,list,tuple)):
+            epochs = [epochs for _ in range(begin,end)]
+        elif(len(epochs) != (end - begin + 1)):
+            epochs += [epochs[-1] for _ in range(end-len(epochs)+2)] 
+        for index,img_size in tqdm(enumerate(range(current_size,self.max_resolution,2),initial=current_size)):
+            loader = torch.utils.data.DataLoader(dataset,batch_size=batch_size,shuffle=shuffle,num_workers=num_workers)
+            save_num = len(loader) // image_num
+            self.netG.sample_size = len(loader)
+            self.netD.sample_size = len(loader)
+            self.train(loader,epochs[index],img_size,save_num,is_tensorboard) 
+
+        if(is_tensorboard):
+            self.log_writer.close()

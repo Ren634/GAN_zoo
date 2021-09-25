14b24b6eee46fe57ceb1c8633f3ebcdda2820b6a
Ren634 <ren.putiputi.634@gmail.com>
2021-09-25 09:20:49 +0900

    Use GPU instead of CPU

--- test/PGGAN_D_test.py
+++ test/PGGAN_D_test.py
@@ -3,8 +3,7 @@ import torch
 from PGGAN import Discriminator
 # %%
 x = torch.randn(size=(1,3,4,4)).to("cuda")
-netD = Discriminator(is_spectral_norm=False).to("cuda")
-
+netD = Discriminator(max_resolution=16,is_spectral_norm=False).to("cuda")
 # %%
 y = netD(x)
 # %%

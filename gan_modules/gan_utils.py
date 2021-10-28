a38e3e81bd176b79258b412bb077f89853f5dc64
Ren634 <ren.putiputi.634@gmail.com>
2021-10-29 02:12:43 +0900

    Apply formatting

--- gan_modules/gan_utils.py
+++ gan_modules/gan_utils.py
@@ -10,28 +10,30 @@ import os
 
 device = "cuda" if torch.cuda.is_available else "cpu"
 
-def save_img(imgs,file_name,img_format="png",is_grid=True):
+
+def save_img(imgs, file_name, img_format="png", is_grid=True):
     today = datetime.date.today().strftime("%Y-%m-%d")
     if(is_grid):
-        imgs = torchvision.utils.make_grid(imgs,nrow=(len(imgs)//4))
-        file_path = f"./logs/imgs/{today}_{file_name}."+img_format
-        os.makedirs("./logs/imgs",exist_ok=True)
+        imgs = torchvision.utils.make_grid(imgs, nrow=(len(imgs) // 4))
+        file_path = f"./logs/imgs/{today}_{file_name}." + img_format
+        os.makedirs("./logs/imgs", exist_ok=True)
     else:
-        file_path = f"./logs/check_imgs/{today}_{file_name}."+img_format
-        os.makedirs("./logs/check_imgs",exist_ok=True)
-        imgs = imgs.squeeze(imgs)
+        file_path = f"./logs/check_imgs/{today}_{file_name}." + img_format
+        os.makedirs("./logs/check_imgs", exist_ok=True)
+        imgs = imgs.squeeze()
     imgs = TF.to_pil_image(imgs)
     imgs.save(file_path)
 
+
 class DataLoader(torch.utils.data.Dataset):
-    def __init__(self,path,resolution,data_format="jpg"):
+    def __init__(self, path, resolution, data_format="jpg"):
         super().__init__()
-        if(path[-1]=='/'):
-            file_paths = path +"*."+data_format
+        if(path[-1] == '/'):
+            file_paths = path + "*." + data_format
         else:
-            file_paths = path +"/*."+data_format
+            file_paths = path + "/*." + data_format
         self.resolution = resolution
-        self.file_names = glob.glob(file_paths,recursive=True)
+        self.file_names = glob.glob(file_paths, recursive=True)
         self.file_paths = path
 
     def __len__(self):
@@ -41,31 +43,33 @@ class DataLoader(torch.utils.data.Dataset):
         label = 1
         img = torchvision.io.read_image(self.file_names[index])
         img = TF.resize(img, size=self.resolution)
-        img = (img - 127.5)/127.5
-        return img,label
+        img = (img - 127.5) / 127.5
+        return img, label
+
 
 class EMA:
-    def __init__(self,weight_decay=0.995):
-        self.weight_decay= weight_decay
+    def __init__(self, weight_decay=0.995):
+        self.weight_decay = weight_decay
         self.__iter_counter = 0
 
-    def setup(self,mvag_net):
+    def setup(self, mvag_net):
         for params in mvag_net.parameters():
             params.requires_grad = False
 
     @property
     def iter_counter(self):
-        return  self.__iter_counter
+        return self.__iter_counter
 
     @iter_counter.setter
-    def iter_counter(self,values):
+    def iter_counter(self, values):
         self.__iter_counter = values
 
-    def apply(self,net,mvag_net):
-        beta = min(1-(1/(self.__iter_counter+1)),self.weight_decay)
-        for params,mvag_params in zip(net.parameters(),mvag_net.parameters()):
-            mvag_params.data = mvag_params.data* beta + (1-beta)*params.data
+    def apply(self, net, mvag_net):
+        beta = min(1 - (1 / (self.__iter_counter + 1)), self.weight_decay)
+        for params, mvag_params in zip(net.parameters(), mvag_net.parameters()):
+            mvag_params.data = mvag_params.data * beta + (1 - beta) * params.data
         self.iter_counter += 1
+
 
 class GAN:
 
@@ -73,27 +77,39 @@ class GAN:
         self.total_epochs = 0
         self.total_steps = 0
         self.params = {}
-        os.makedirs(f"./params",exist_ok=True)
+        os.makedirs("./params", exist_ok=True)
 
-    def save_model(self,save_path):
+    def train_d(self):
+        raise NotImplementedError
+
+    def train_g(self):
+        raise NotImplementedError
+
+    def fit(self):
+        raise NotImplementedError
+
+    def generate(self):
+        raise NotImplementedError
+
+    def save_model(self, save_path):
         params = {
-                "model_state_d":self.netD.state_dict(),
-                "optim_state_d":self.netD.optimizer.state_dict(),
-                "model_state_g":self.netG.state_dict(),
-                "optim_state_g":self.netG.optimizer.state_dict(),
-                "total_epochs":self.total_epochs,
-                "total_steps":self.total_steps,
-                "fixed_noise":self.fixed_noise,
+            "model_state_d": self.netD.state_dict(),
+            "optim_state_d": self.netD.optimizer.state_dict(),
+            "model_state_g": self.netG.state_dict(),
+            "optim_state_g": self.netG.optimizer.state_dict(),
+            "total_epochs": self.total_epochs,
+            "total_steps": self.total_steps,
+            "fixed_noise": self.fixed_noise,
         }
         self.params.update(params)
         if(save_path[-3:] != ".pt"):
             save_path = save_path + ".pt"
-        torch.save(params,save_path)
+        torch.save(self.params, save_path)
 
-    def load_model(self,load_path):
+    def load_model(self, load_path):
         if(load_path[-3:] != ".pt"):
             load_path = load_path + ".pt"
-        params = torch.load(load_path+".pt")
+        params = torch.load(load_path)
         self.netD.load_state_dict(params["model_state_d"])
         self.netG.load_state_dict(params["model_state_g"])
         self.netD.optimizer.load_state_dict(params["optim_state_d"])
@@ -102,52 +118,59 @@ class GAN:
         self.total_epochs = params["total_epochs"]
         self.fixed_noise = params["fixed_noise"]
 
+
 def random_translation(inputs):
-    b,_,h,w = inputs.shape
-    y,x = torch.randint(-h//8,h//8,size=(b,1,1)),torch.randint(-w//8,w//8,size=(b,1,1))
-    index_b,index_y,index_x = torch.meshgrid(
-        torch.arange(b,dtype=torch.long),
-        torch.arange(h,dtype=torch.long),
-        torch.arange(w,dtype=torch.long)
+    b, _, h, w = inputs.shape
+    y, x = torch.randint(-h // 8, h // 8, size=(b, 1, 1)), torch.randint(-w // 8, w // 8, size=(b, 1, 1))
+    index_b, index_y, index_x = torch.meshgrid(
+        torch.arange(b, dtype=torch.long),
+        torch.arange(h, dtype=torch.long),
+        torch.arange(w, dtype=torch.long)
     )
-    index_y = torch.clamp(index_y + y , min=0, max=h+1)
-    index_x = torch.clamp(index_x + x , min=0, max=w+1)
-    inputs = F.pad(inputs,[1,1,1,1,0,0,0,0]) # dim -1(left,right) dim -2(left, right) # dim -3(left, right) -4(left, right)
-    return inputs.permute(0,2,3,1).contiguous()[index_b,index_y,index_x].permute(0,3,1,2).contiguous()
+    index_y = torch.clamp(index_y + y, min=0, max=h + 1)
+    index_x = torch.clamp(index_x + x, min=0, max=w + 1)
+    inputs = F.pad(inputs, [1, 1, 1, 1, 0, 0, 0, 0])  # dim -1(left,right) dim -2(left, right) # dim -3(left, right) -4(left, right)
+    return inputs.permute(0, 2, 3, 1).contiguous()[index_b, index_y, index_x].permute(0, 3, 1, 2).contiguous()
+
 
 def random_cutout(inputs):
-    b,_,h,w = inputs.shape
-    y,x= torch.randint(10,h//3,size=(1,)),torch.randint(10,w//3,size=(1,))
-    index_b,index_y,index_x = torch.meshgrid(
-        torch.arange(b,dtype=torch.long),
-        torch.arange(y.item(),dtype=torch.long),
-        torch.arange(x.item(),dtype=torch.long)
+    b, _, h, w = inputs.shape
+    y, x = torch.randint(10, h // 3, size=(1,)), torch.randint(10, w // 3, size=(1,))
+    index_b, index_y, index_x = torch.meshgrid(
+        torch.arange(b, dtype=torch.long),
+        torch.arange(y.item(), dtype=torch.long),
+        torch.arange(x.item(), dtype=torch.long)
     )
-    index_y = torch.clamp(index_y + torch.randint(0,h-10,size=(b,1,1)),min=0,max=h-1)
-    index_x = torch.clamp(index_x + torch.randint(0,w-10,size=(b,1,1)),min=0,max=w-1)
-    mask = torch.ones((b,h,w),device=inputs.device,dtype=inputs.dtype)
-    mask[index_b,index_y,index_x] *= 0
+    index_y = torch.clamp(index_y + torch.randint(0, h - 10, size=(b, 1, 1)), min=0, max=h - 1)
+    index_x = torch.clamp(index_x + torch.randint(0, w - 10, size=(b, 1, 1)), min=0, max=w - 1)
+    mask = torch.ones((b, h, w), device=inputs.device, dtype=inputs.dtype)
+    mask[index_b, index_y, index_x] *= 0
     return inputs * mask.unsqueeze(1)
 
+
 def random_saturation(inputs):
-    mean = torch.mean(inputs,dim=1,keepdim=True,dtype=inputs.dtype)
-    factor = torch.rand(size=(inputs.shape[0],1,1,1),dtype=inputs.dtype,device=inputs.device) * 2
+    mean = torch.mean(inputs, dim=1, keepdim=True, dtype=inputs.dtype)
+    factor = torch.rand(size=(inputs.shape[0], 1, 1, 1), dtype=inputs.dtype, device=inputs.device) * 2
     return (inputs - mean) * factor + mean
+
 
 def random_brightness(inputs):
-    factor = torch.rand(size=(inputs.shape[0],1,1,1),dtype=inputs.dtype,device=inputs.device) / 4
+    factor = torch.rand(size=(inputs.shape[0], 1, 1, 1), dtype=inputs.dtype, device=inputs.device) / 4
     return inputs + factor
 
+
 def random_contrast(inputs):
-    b,c,h,w = inputs.shape
-    mean = torch.mean(inputs,dim=(1,2,3),keepdim=True)
-    factor = torch.rand(size=(b,1,1,1),dtype=inputs.dtype,device=inputs.device) + 0.5
+    b, c, h, w = inputs.shape
+    mean = torch.mean(inputs, dim=(1, 2, 3), keepdim=True)
+    factor = torch.rand(size=(b, 1, 1, 1), dtype=inputs.dtype, device=inputs.device) + 0.5
     return (inputs - mean) * factor + mean
 
+
 class AdaptiveDA(nn.Module):
-    def __init__(self,frequency=4,threshold=0.6):
-        self.prob = torch.tensor(0)
-        self.threshold = torch.tensor(threshold)
+    def __init__(self, frequency=4, threshold=0.6, device="cpu"):
+        super().__init__()
+        self.prob = torch.tensor(0., device=device)
+        self.threshold = threshold
         self.frequency = torch.tensor(frequency)
         self.n = torch.tensor(0)
         self.functions = [
@@ -164,7 +187,7 @@ class AdaptiveDA(nn.Module):
         return self._batch_size
 
     @batch_size.setter
-    def batch_size(self,value):
+    def batch_size(self, value):
         self._batch_size = value
 
     @property
@@ -172,23 +195,23 @@ class AdaptiveDA(nn.Module):
         return self._epoch
 
     @epoch.setter
-    def epoch(self,value):
+    def epoch(self, value):
         self._epoch = value
 
-    def adjust_p(self,real_logit):
+    def adjust_p(self, real_logit):
+        if(self.n != self.frequency):
+            return None
         rt = torch.sign(real_logit).mean()
-        self.prob += torch.sign(rt - self.threshold)*(self.epoch*self.batch_size)/(self.batch_size*1000)
-        self.prob = torch.clamp(self.prob,min=0,max=1)
+        self.prob += torch.sign(rt - self.threshold) * (self.epoch * self.batch_size) / (self.batch_size * 1000)
+        self.prob = torch.clamp(self.prob, min=0, max=1)
+        self.n = 0
 
-    def forward(self,x,target=False):
+    def forward(self, x, target=False):
         if(target):
             self.n += 1
-            if(self.n == self.frequency):
-                self.adjust_p(x)
-                self.n = 0
         prob = self.prob.item()
-        is_applying_list = np.random.choice(2,size=len(self.functions),p=[prob,1-prob])
-        for function,is_applying in enumerate(self.functions,is_applying_list):
+        is_applying_list = np.random.choice([0, 1], size=len(self.functions), p=[1 - prob, prob])
+        for function, is_applying in zip(self.functions, is_applying_list):
             if(is_applying):
                 x = function(x)
         return x
--- gan_modules/losses.py
+++ gan_modules/losses.py
@@ -3,33 +3,36 @@ from torch import nn
 from torch.nn import functional as F
 import numpy as np
 
-def hinge(real,fake):
-    output = F.relu(1-real).mean() + F.relu(1+fake).mean()
-    return output 
-    
+
+def hinge(real, fake):
+    output = F.relu(1 - real).mean() + F.relu(1 + fake).mean()
+    return output
+
+
 class Hinge(nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(self,real,fake,*imgs):
-        return hinge(real,fake)
+    def forward(self, real, fake, *imgs):
+        return hinge(real, fake)
+
 
 class WassersteinGP(nn.Module):
-    def __init__(self,net,penalty_coef=10):
+    def __init__(self, net, penalty_coef=10):
         super().__init__()
         self.penalty_coef = penalty_coef
         self.net = net
 
-    def forward(self,real,fake,*imgs):
-        real_img,fake_img = imgs
+    def forward(self, real, fake, *imgs):
+        real_img, fake_img = imgs
         batch_size = real.shape[0]
         coef = torch.tensor(
-            np.random.uniform(size=(batch_size,1,1,1)),
+            np.random.uniform(size=(batch_size, 1, 1, 1)),
             requires_grad=True,
             device=real.device,
             dtype=real.dtype
-            )
-        penalty_input = real_img * coef + (1 - coef)*fake_img
+        )
+        penalty_input = real_img * coef + (1 - coef) * fake_img
         penalty_output = self.net(penalty_input)
         gradient = torch.autograd.grad(
             penalty_output,
@@ -37,13 +40,8 @@ class WassersteinGP(nn.Module):
             grad_outputs=torch.ones_like(penalty_output),
             create_graph=True,
             retain_graph=True
-            )[0]
-        penalty = gradient.view(batch_size,-1)
-        penalty = torch.square(torch.norm(gradient,dim=1)-1)
+        )[0]
+        penalty = gradient.view(batch_size, -1)
+        penalty = torch.square(torch.norm(gradient, dim=1) - 1)
         output = fake.mean() - real.mean() + self.penalty_coef * penalty.mean()
         return output
-
-        
-
-
-        
